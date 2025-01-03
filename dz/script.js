bafdbd435c20c78c317227ea81a0c2c39e0331cf
lyaxsash <ruki.kune@yandex.ru>
2025-01-03 21:30:13 +0300

    dz fin

--- dz/script.js
+++ dz/script.js
@@ -1,11 +1,11 @@
-import {greet} from "./name"; 
-import {celsiusToFahrenheit} from "./celsius_farenheit"; 
-import {calculateFallDistance} from "./distance";
-import {calculateAverage} from "./average_value";
-import {concatStrings} from "./words";
+import {greet} from "./name.js"; 
+import {celsiusToFahrenheit} from "./celsius_farenheit.js"; 
+import {calculateFallDistance} from "./distance.js";
+import {calculateAverage} from "./average_value.js";
+import {concatStrings} from "./words.js";
 
 console.log(greet("Alex"))
-console.log(celsiusToFahrenheit(1));
+console.log(celsiusToFahrenheit(7));
 console.log(calculateFallDistance(2));
-console.log(calculateAverage(4,6,5));
+console.log(calculateAverage(4,6,56));
 console.log(concatStrings("дороже второго", "съела корова"));
--- dz/words.js
+++ dz/words.js
@@ -1,4 +1,4 @@
 // Два слова
 export function concatStrings(word1, word2) {
-	alert(`Первое слово - «${word1}», Второе слово - «${word2}»`)
+	return `Первое слово - «${word1}», Второе слово - «${word2}»`
 }
